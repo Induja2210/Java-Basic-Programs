3c59c4ba7f146bd3ff241a96466f9f4f090a442d
Induja B <induja437@gmail.com>
2025-09-21 21:13:39 +0530

    Add files via upload

---
+++ Multiplicationtable.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Multiplicationtable {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int a = sc.nextInt();
+        for (int i=0; i<=10; i++){
+        System.out.println(a +"*" +i +"=" + (a*i));
+        }
+        sc.close();
+    }
+    
+}
