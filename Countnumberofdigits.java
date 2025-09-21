463a66bb982255233cdd5c6ee04913b90f8a751c
Induja B <induja437@gmail.com>
2025-09-21 17:42:33 +0530

    Add files via upload

---
+++ Countnumberofdigits.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Countnumberofdigits{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");        
+        int n=sc.nextInt();
+        int count=0;
+        if(n==0)count=1;
+            else{
+                while(n!=0){
+                    count++;
+                    n/=10;
+                }
+            }
+
+        
+        System.out.println("Number of digits" +count);
+        
+        sc.close();
+
+    }
+}
